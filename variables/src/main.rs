97158ca9d5afbaf04403c85f33b2554dd3b32ce2
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-02-05 13:35:02 +0200

    finish rustling variables exercises

---
+++ variables/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is {}", x);
+
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {quotient}");
+
+    let truncated = -5 / 3;
+    println!("truncated: {truncated}");
+}
