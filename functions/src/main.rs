b183e6dc4e104991390a712a73c50527f622223d
jovani <163670507+lipatovSergey@users.noreply.github.com>
2026-02-08 18:44:57 +0200

    finish functions lesson from rust book

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
 
-    another_function()
+    let x = five();
+
+    println!("The value is {x}");
+
+    let y = plus_one(5);
+
+    println!("The value is {y}")
 }
 
-fn another_function() {
-    println!("Another function.")
+fn another_function(x: i32) {
+    println!("Passed value is: {x}")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is {value}{unit_label}")
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
